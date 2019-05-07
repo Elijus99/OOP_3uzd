6dbb537c624b04585a72a81fb8aec12f7da9aa42
Elijus99 <47322512+Elijus99@users.noreply.github.com>
2019-05-07 09:58:50 +0300

    Delete functions_c.js

--- search/functions_c.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['_7estudentbase',['~StudentBase',['../class_student_base.html#ad909509549288293e4af9ed332c5b156',1,'StudentBase']]],
-  ['_7estudentderived',['~StudentDerived',['../class_student_derived.html#ab82e183e7428ca1f19f2219bc3b119f4',1,'StudentDerived']]]
-];

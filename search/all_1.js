00f5dc75e6267a214d7162a1e2833effd09566a5
Elijus99 <47322512+Elijus99@users.noreply.github.com>
2019-05-07 09:49:01 +0300

    Delete all_1.js

--- search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['division',['division',['../functions_8cpp.html#a6e672efd1fdabafe6dac68416c51e958',1,'division(int a, int b):&#160;functions.cpp'],['../functions_8h.html#a6e672efd1fdabafe6dac68416c51e958',1,'division(int a, int b):&#160;functions.cpp']]]
-];

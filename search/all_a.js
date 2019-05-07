538cc0c7cecbfe337ca4b0cc3d69e531497aa388
Elijus99 <47322512+Elijus99@users.noreply.github.com>
2019-05-07 09:51:38 +0300

    Delete all_a.js

--- search/all_a.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['pav',['pav',['../class_student_base.html#abdccea3079f2c5df94f35525d4c5c75d',1,'StudentBase']]],
-  ['pavarde',['pavarde',['../class_student_base.html#adc52417e044a31212359ea106fe81236',1,'StudentBase']]],
-  ['printinfo',['printInfo',['../class_student_derived.html#a7c84acecf565c70e2a6d4bfd1c60f7c1',1,'StudentDerived']]]
-];

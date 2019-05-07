5035905a87fcfd5bb34e4651c495ee46888bd05f
Elijus99 <47322512+Elijus99@users.noreply.github.com>
2019-05-07 09:49:17 +0300

    Delete all_2.js

--- search/all_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['egzaminas',['egzaminas',['../class_student_base.html#a250eda1d2180e7d61ca3f3930417cf06',1,'StudentBase']]],
-  ['egzaminas_5f',['egzaminas_',['../class_student_base.html#a570c3b4ff3d867280b15cf4d680a6cf7',1,'StudentBase']]],
-  ['exist',['exist',['../functions_8cpp.html#ada10f1c4fcddbd70e2f5d396db0fdb86',1,'exist(const string &amp;fileName):&#160;functions.cpp'],['../functions_8h.html#ada10f1c4fcddbd70e2f5d396db0fdb86',1,'exist(const string &amp;fileName):&#160;functions.cpp']]]
-];

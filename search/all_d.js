c248f0fcd9222cf6e7fd5ca333613ce57c2dc7c8
Elijus99 <47322512+Elijus99@users.noreply.github.com>
2019-05-07 09:52:30 +0300

    Delete all_d.js

--- search/all_d.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['vard',['vard',['../class_student_base.html#a43bacd79e942bdf9d2fb984d08edf454',1,'StudentBase']]],
-  ['vardas',['vardas',['../class_student_base.html#a6fe791399496f3b4b0c9d910472b2232',1,'StudentBase']]]
-];

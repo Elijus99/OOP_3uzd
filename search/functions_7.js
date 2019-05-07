9f393cd6329734fac4c105294a70bef5895282e1
Elijus99 <47322512+Elijus99@users.noreply.github.com>
2019-05-07 09:57:42 +0300

    Delete functions_7.js

--- search/functions_7.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['operator_3d',['operator=',['../class_student_base.html#a33492cade3cb6912b66b0aaac26ca0bc',1,'StudentBase::operator=()'],['../class_student_derived.html#aec705037a02dabdf675f62420d5fd611',1,'StudentDerived::operator=()']]],
-  ['outputtofiles',['OutputToFiles',['../functions_8cpp.html#a4f7f9b85b2b531e2102f3b1208c26ae9',1,'OutputToFiles(std::vector&lt; StudentDerived &gt; &amp;ls, const std::vector&lt; StudentDerived &gt; &amp;vargs, const string &amp;GType, int VSize, int PSize):&#160;functions.cpp'],['../functions_8h.html#a4f7f9b85b2b531e2102f3b1208c26ae9',1,'OutputToFiles(std::vector&lt; StudentDerived &gt; &amp;ls, const std::vector&lt; StudentDerived &gt; &amp;vargs, const string &amp;GType, int VSize, int PSize):&#160;functions.cpp']]]
-];

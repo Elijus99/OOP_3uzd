0af6b7483c089d0dce14d5493b51ff0f486a1dff
Elijus99 <47322512+Elijus99@users.noreply.github.com>
2019-05-07 09:51:00 +0300

    Delete all_8.js

--- search/all_8.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['nd',['nd',['../class_student_base.html#aff680a22b1f9240273612335b94f58c4',1,'StudentBase']]],
-  ['nd_5f',['nd_',['../class_student_base.html#ab55fd8db43909de908c66ed7f3bdd4e6',1,'StudentBase']]]
-];

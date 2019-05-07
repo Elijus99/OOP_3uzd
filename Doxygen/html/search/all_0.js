1317ddacee7f94d8a67b4840ff3ef0cedc76207c
Elijus99 <47322512+Elijus99@users.noreply.github.com>
2019-05-07 09:29:35 +0300

    Fixed doxygen

--- Doxygen/html/search/all_0.js
+++ Doxygen/html/search/all_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['studentbase',['StudentBase',['../class_student_base.html',1,'']]],
-  ['studentderived',['StudentDerived',['../class_student_derived.html',1,'']]]
+  ['clearnd',['clearNd',['../class_student_base.html#a3776b744979f4f34968ef407503f44a8',1,'StudentBase']]],
+  ['comparealphabet',['compareAlphabet',['../student_8cpp.html#ab3a06bf19cad990b98117ad89f80a2fe',1,'compareAlphabet(const StudentBase &amp;a, const StudentBase &amp;b):&#160;student.cpp'],['../student_8h.html#ab3a06bf19cad990b98117ad89f80a2fe',1,'compareAlphabet(const StudentBase &amp;a, const StudentBase &amp;b):&#160;student.cpp']]]
 ];
---
+++ Doxygen/html/search/all_8.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['nd',['nd',['../class_student_base.html#aff680a22b1f9240273612335b94f58c4',1,'StudentBase']]],
+  ['nd_5f',['nd_',['../class_student_base.html#ab55fd8db43909de908c66ed7f3bdd4e6',1,'StudentBase']]]
+];
